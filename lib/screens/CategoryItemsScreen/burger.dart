a2ab09de1ad66e4da11d858af9ac2dfd2adaa835
mac <mac@macs-MacBook-Pro.local>
2023-09-18 23:38:50 +0500

    add screen

---
+++ lib/screens/CategoryItemsScreen/burger.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BurgerScreen extends StatefulWidget {
+  const BurgerScreen({super.key});
+
+  @override
+  State<BurgerScreen> createState() => _BurgerScreenState();
+}
+
+class _BurgerScreenState extends State<BurgerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Container(
+        child: Text("Burger Screen"),
+      ),
+    ));
+  }
+}
